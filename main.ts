a434a5ad575949c8cae951ef7451e8417acfdd26
sunzhangchang <642311409@qq.com>
2022-10-22 15:07:08 +0800

    log

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
 import { serve } from 'https://deno.land/std@0.160.0/http/server.ts'
 
 serve(req => {
-  console.log(req)
+  console.log('in:', req)
 
   const url = new URL(req.url)
   url.host = Deno.env.get('DOMAIN') ?? url.host
 
-  console.log(url)
+  // console.log(url)
 
-  // req.body
-  return fetch(url.href, {
+  const reqnew: Request = {
     ...req,
     url: url.href,
-  } as Request)
+  } as Request
+
+  console.log('out:', reqnew)
+
+  return fetch(url.href, reqnew)
 })
